2e1e657f87c94ee01bc9867a51d215312b638173
Petr Šťastný <petr@soptik.tech>
2022-06-08 11:10:47 +0200

    Added example page with listing, creation and input validation

--- Pages/Administration/Seasons.cshtml.cs
+++ Pages/Administration/Seasons.cshtml.cs
@@ -1,11 +1,39 @@
+using Fiks.Data;
+using Fiks.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
-namespace fiks_web.Pages.Administration;
+namespace Fiks.Pages.Administration;
 
-public class Seasons : PageModel
+public class SeasonsModel : PageModel
 {
-    public void OnGet()
+    private readonly ILogger logger;
+    private readonly SeasonContext context;
+
+    public SeasonsModel(ILogger<SeasonsModel> logger, SeasonContext context)
     {
+        this.logger = logger;
+        this.context = context;
+    }
+
+    [BindProperty]
+    public Season Season { get; set; }
+    public IList<Season> Seasons;
+    
+    public IActionResult OnGet()
+    {
+        Seasons = context.Season.ToList();
+        return Page();
+    }
+
+    public IActionResult OnPost()
+    {
+        if (!ModelState.IsValid) return OnGet();
         
+        var ent = context.Season.Add(Season);
+        context.SaveChanges();
+
+        return RedirectToPage("./Seasons", ent.Entity.ID);
     }
 }
