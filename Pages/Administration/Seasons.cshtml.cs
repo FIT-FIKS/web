ecfdf874d4fd0ee8593c68af5071e5eaf282cf4e
Petr Šťastný <petr@soptik.tech>
2022-06-08 11:55:50 +0200

    Added comments

--- Pages/Administration/Seasons.cshtml.cs
+++ Pages/Administration/Seasons.cshtml.cs
@@ -8,7 +8,9 @@ namespace Fiks.Pages.Administration;
 
 public class SeasonsModel : PageModel
 {
+    /// Instance of object that can be used for logging
     private readonly ILogger logger;
+    /// Database context, can be used for querying and modifications
     private readonly SeasonContext context;
 
     public SeasonsModel(ILogger<SeasonsModel> logger, SeasonContext context)
@@ -17,16 +19,22 @@ public class SeasonsModel : PageModel
         this.context = context;
     }
 
+    /// Object that is sent from frontend and generated from filling a form.
     [BindProperty]
     public Season Season { get; set; }
+    /// List of objects that are passed to frontend and filled into a table.
+    /// User can list through them and display their detail (such as rounds in given season)
     public IList<Season> Seasons;
     
+    /// Return all seasons in database
     public IActionResult OnGet()
     {
         Seasons = context.Season.ToList();
         return Page();
     }
 
+    /// User sent a post request. Verify that it contains a valid Season object, and if so, insert
+    /// it into database.
     public IActionResult OnPost()
     {
         if (!ModelState.IsValid) return OnGet();
