52cf2f971269ca4c510f036d3edd18a1a166a66d
Pavel Baštinec <bastipav@fit.cvut.cz>
2022-06-09 23:33:20 +0200

    AddSchoolModel

--- Data/FiksDbContext.cs
+++ Data/FiksDbContext.cs
@@ -7,4 +7,13 @@ public class FiksDbContext : DbContext
     public FiksDbContext(DbContextOptions<FiksDbContext> options) : base(options) { }
 
     public DbSet<Models.Season> Season { get; set; }
+
+    public DbSet<Models.School> School { get; set; }
+
+    protected override void OnModelCreating(Microsoft.EntityFrameworkCore.ModelBuilder b) {
+        b.Entity<Models.School>(o => {
+            o.Property(v => v.Validated)
+                .HasDefaultValue(false);
+        });
+    }
 }
