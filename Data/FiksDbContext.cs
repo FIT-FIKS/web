6aac8dbdbe91fb5f1235395063552f820fac2241
Pavel Baštinec <bastipav@fit.cvut.cz>
2022-06-10 02:26:12 +0200

    add-user

--- Data/FiksDbContext.cs
+++ Data/FiksDbContext.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace Fiks.Data;
@@ -10,11 +11,46 @@ public class FiksDbContext : DbContext
 
     public DbSet<Models.School> School { get; set; }
 
+    public DbSet<Models.User> User { get; set; }
+
+    public DbSet<IdentityUserLogin<long>> UserLogin { get; set; }
+
+    public DbSet<IdentityUserToken<long>> UserToken { get; set; }
+
     protected override void OnModelCreating(Microsoft.EntityFrameworkCore.ModelBuilder b) {
         b.Entity<Models.School>(o => {
             o.Property(v => v.Validated)
                 .HasDefaultValue(false);
         });
+
+        b.Entity<Models.User>(o =>
+        {
+            o.Ignore(v => v.AccessFailedCount);
+            o.Ignore(v => v.LockoutEnabled);
+            o.Ignore(v => v.LockoutEnd);
+            o.Ignore(v => v.PhoneNumber);
+            o.Ignore(v => v.PhoneNumberConfirmed);
+            o.Ignore(v => v.TwoFactorEnabled);
+
+            o.HasKey(v => v.Id);
+            o.HasIndex(v => v.NormalizedUserName).HasDatabaseName("UserNameIndex");
+            o.HasIndex(v => v.NormalizedEmail).HasDatabaseName("EmailIndex");
+
+            o.Property(v => v.ConcurrencyStamp).IsConcurrencyToken();
+            o.Property(v => v.UserName).HasMaxLength(80).IsRequired();
+            o.Property(v => v.NormalizedUserName).HasMaxLength(80).IsRequired();
+            o.Property(v => v.Email).HasMaxLength(80).IsRequired();
+            o.Property(v => v.NormalizedEmail).HasMaxLength(80).IsRequired();
+
+            o.HasMany<IdentityUserLogin<long>>().WithOne().HasForeignKey(ul => ul.UserId).IsRequired();
+            o.HasMany<IdentityUserToken<long>>().WithOne().HasForeignKey(ut => ut.UserId).IsRequired();
+        });
+
+        b.Entity<IdentityUserLogin<long>>()
+            .HasKey(v => new { v.LoginProvider, v.ProviderKey });
+
+        b.Entity<IdentityUserToken<long>>()
+            .HasKey(v => new { v.UserId, v.LoginProvider, v.Name });
 
         base.OnModelCreating(b);
     }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Fiks.Data;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +17,42 @@ else
 {
     // TODO: load database connection string from environmental variable
 }
+
+builder.Services.AddIdentityCore<Fiks.Models.User>(o =>
+{
+    o.Password.RequireDigit = false;
+    o.Password.RequireLowercase = false;
+    o.Password.RequireNonAlphanumeric = false;
+    o.Password.RequireUppercase = false;
+    o.Password.RequiredLength = 5;
+    o.Password.RequiredUniqueChars = 0;
+
+    /*o.User.AllowedUserNameCharacters =
+      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";*/ // If you want to allow only some chars for username
+})
+    .AddUserStore<UserStore<Fiks.Models.User, IdentityRole<long>, FiksDbContext, long>>()
+    .AddDefaultTokenProviders()
+    .AddSignInManager<SignInManager<Fiks.Models.User>>();
+
+builder.Services.AddAuthentication(o =>
+{
+    o.DefaultAuthenticateScheme = IdentityConstants.ApplicationScheme;
+    o.DefaultChallengeScheme = IdentityConstants.ApplicationScheme;
+    o.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+}).AddCookie(IdentityConstants.ApplicationScheme, o =>
+{
+    o.LogoutPath = new PathString("/Account/Login"); // todo: změnit login path soptíku :)
+    o.Events = new Microsoft.AspNetCore.Authentication.Cookies.CookieAuthenticationEvents()
+    {
+        OnValidatePrincipal = SecurityStampValidator.ValidatePrincipalAsync
+    };
+}).AddCookie(IdentityConstants.ExternalScheme, o =>
+{
+    o.Cookie.Name = IdentityConstants.ExternalScheme;
+    o.ExpireTimeSpan = TimeSpan.FromMinutes(5.0);
+});
+
+builder.Services.AddScoped<ISecurityStampValidator, SecurityStampValidator<Fiks.Models.User>>();
 
 var app = builder.Build();
 
