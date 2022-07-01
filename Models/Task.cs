dbe811697e09ad21cee5f7e83ecdae39437546f8
Pavel Baštinec <bastipav@fit.cvut.cz>
2022-07-01 02:50:08 +0200

    UpdateTaskModel

--- Models/Task.cs
+++ Models/Task.cs
@@ -1,18 +1,28 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace Fiks.Models
 {
     public class Task
     {
+        [Key]
         public int Id { get; set; }
 
+        [Required]
+        [StringLength(80)]
         public string Content { get; set; }
 
+        [Required]
         public int MaxScore { get; set; }
 
+        [Required]
         public File ReferenceSolution { get; set; }
 
+        [Required]
+        [StringLength(80)]
         public string Title { get; set; }
 
+        [Required]
         public TaskType Type { get; set; }
     }
 
